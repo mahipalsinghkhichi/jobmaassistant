cd070d3ce7394c8ebef725f5cc5f56756224f37d
mahipal singh khichi <mahipalsingh2032@gmail.com>
2023-09-04 11:34:13 +0530

    myproject deleted

--- myproject/myapp/tasks.py
+++
@@ -1,7 +0,0 @@
-# myapp/tasks.py
-from celery import shared_task
-from addingBot.main import main
-
-@shared_task
-def start_bot_recording(id):
-    main(id)
--- myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('startRecording/', startRecording ),
-    path('stopRecording/', stopRecording ),
-    path('getTranscripts/', getTranscripts ),
-    # path('startRecordingWithLastURlForTesting/', startRecordingWithLastURlForTesting ),
-]
